4f48234f23337d9c32203fbf42a736eec4219027
Craig Wilson <craig@wilsoncg.net>
2020-07-30 13:08:45 +0100

    capture of jekyll site

---
+++ _jekyllsite/assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Jekyll up and running",
+        "excerpt":"Jekyll is a tool to create static web pages generated from markdown files. This means you don’t have to worry about paying for wordpress hosting, or worse setting up a LAMP server, with MySQL/PHP just to host a blog. It’s got some really useful features, such as syntax highlighting for...","categories": ["jekyll"],
+        "tags": ["jekyll","front matter","getting started","github pages"],
+        "url": "http://0.0.0.0:4000/jekyll/welcome-to-jekyll/",
+        "teaser": null
+      },{
+        "title": "Down the rabbit hole",
+        "excerpt":"“Just setup jekyll on your local windows 10 machine” they said, how hard can it be? You’ve installed ruby, then jekyll. Created a site, found a theme, installed some gems, bundled something and now you have some html served up on port 4000. But wait a minute, now your console...","categories": ["jekyll"],
+        "tags": ["jekyll","jekyll themes","remote themes","github pages","open source","msys2","mingw","gcc","curl","libcurl","winssl"],
+        "url": "http://0.0.0.0:4000/jekyll/down-the-rabbit-hole/",
+        "teaser": null
+      },{
+        "title": "Don't fear the monad",
+        "excerpt":"A bunch of useful links covering monads in F# and C#.   Videos   What is a Monad?      Brian Beckman - Don’t fear the Monad      Links   https://blogs.msdn.microsoft.com/wesdyer/2008/01/10/the-marvels-of-monads/   https://github.com/fsprojects/FSharpx.Extras/blob/master/tests/FSharpx.CSharpTests/ValidationExample.cs   http://blog.leifbattermann.de/2015/09/12/error-handling-with-applicative-functors-in-f-and-c/   http://bugsquash.blogspot.nl/2011/08/validating-with-applicative-functors-in.html   http://hestia.typepad.com/flatlander/2011/06/partial-function-application-in-f-index.html   http://codebetter.com/matthewpodwysocki/2010/01/18/much-ado-about-monads-creating-extended-builders/   http://blog.ploeh.dk/2017/01/03/decoupling-application-errors-from-domain-models/   https://fsharpforfunandprofit.com/series/computation-expressions.html  ","categories": ["monads"],
+        "tags": ["monads","monoids","category theory","functional programming","f#","linq"],
+        "url": "http://0.0.0.0:4000/monads/monads/",
+        "teaser": null
+      },{
+        "title": "Docker plus Jekyll equals win",
+        "excerpt":"I’ve started to spend more time looking at docker. A useful excerise has been to use the docker jekyll image to setup a working development environment. Previously, it took me 2 days of configuring my windows 10 desktop with: Correct version of Ruby installed Ruby gems installed &amp; configured correctly...","categories": ["jekyll"],
+        "tags": ["jekyll","docker","github pages"],
+        "url": "http://0.0.0.0:4000/jekyll/docker-plus-jekyll-equals-win/",
+        "teaser": null
+      },{
+        "title": "Remove skype context menu with powershell",
+        "excerpt":"Although powershell is enabled by default when installing Windows Terminal, it seems the development team have overlooked the scenario where you want to run a script as Admin and don’t want to context switch. Adding the below entry into your terminal settings json file will give you a powershell admin...","categories": ["powershell"],
+        "tags": ["powershell","skype","registry","windows terminal"],
+        "url": "http://0.0.0.0:4000/powershell/remove-skype-context-menu/",
+        "teaser": null
+      }]
