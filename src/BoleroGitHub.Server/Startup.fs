74758cd88c032d38edfb87cd5f336e63c30ed249
Craig Wilson <craig@wilsoncg.net>
2020-08-04 17:16:56 +0100

    simplify startup for serving static content

--- src/BoleroGitHub.Server/Startup.fs
+++ src/BoleroGitHub.Server/Startup.fs
@@ -10,42 +10,39 @@ open Bolero
 open Bolero.Remoting.Server
 open Bolero.Server.RazorHost
 open Bolero.Templating.Server
+open System.IO
+open Microsoft.Extensions.FileProviders
+
+// Simplified startup with html loader
+// Refer to bolero tryfsharp sample
+// https://github.com/fsbolero/TryFSharpOnWasm/blob/master/src/WebFsc.Server/Startup.fs
 
 type Startup() =
+    let clientProjPath = Path.Combine(__SOURCE_DIRECTORY__, "..", "BoleroGitHub.Client")
 
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     member this.ConfigureServices(services: IServiceCollection) =
-        services.AddMvc()
-         .AddRazorRuntimeCompilation()
-         .AddRazorPagesOptions(fun o -> 
-            o.Conventions.ConfigureFilter(IgnoreAntiforgeryTokenAttribute()) |> ignore) |> ignore
-        services.AddServerSideBlazor() |> ignore
-        services
-            .AddAuthorization()
-            .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie()
-                .Services
-            .AddBoleroHost()
-#if DEBUG
-            .AddHotReload(templateDir = __SOURCE_DIRECTORY__ + "/../BoleroGitHub.Client")
-#endif
+        services.AddControllers() |> ignore         
+        services.AddHotReload(templateDir = clientProjPath)
         |> ignore
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     member this.Configure(app: IApplicationBuilder, env: IWebHostEnvironment) =
         app
-            .UseAuthentication()
-            .UseRemoting()
-            .UseStaticFiles()            
+            .UseStaticFiles(
+                StaticFileOptions(
+                    FileProvider = new PhysicalFileProvider(
+                        Path.Combine(clientProjPath, "wwwroot")
+                    )
+                )
+            )            
+            .UseRouting()       
             .UseBlazorFrameworkFiles()
-            .UseRouting()
             .UseEndpoints(fun endpoints ->
-#if DEBUG
                 endpoints.UseHotReload()
-#endif
-                endpoints.MapBlazorHub() |> ignore
-                endpoints.MapFallbackToPage("/_Host") |> ignore)
+                endpoints.MapControllers() |> ignore
+                endpoints.MapFallbackToFile("index.html") |> ignore)
         |> ignore
 
 module Program =
