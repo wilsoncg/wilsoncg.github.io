5940189c665529b9620ae8f466aaa705be013671
Craig Wilson <craig@wilsoncg.net>
2020-09-04 18:01:16 +0100

    rework

--- src/BoleroGitHub.Client/Startup.fs
+++ src/BoleroGitHub.Client/Startup.fs
@@ -1,5 +1,6 @@
 namespace BoleroGitHub.Client
 
+open System
 open System.Net.Http
 open Microsoft.AspNetCore.Components.WebAssembly.Hosting
 open Microsoft.Extensions.DependencyInjection
@@ -11,14 +12,15 @@ module Program =
     [<EntryPoint>]
     let Main args =
         let builder = WebAssemblyHostBuilder.CreateDefault(args)
-        printfn "HostEnvironment %s BaseAddress %s" builder.HostEnvironment.Environment builder.HostEnvironment.BaseAddress
-        let http = new HttpClient(BaseAddress = System.Uri(builder.HostEnvironment.BaseAddress))
-        builder.Services.AddSingleton<HttpClient>(fun _ -> http) |> ignore
+        let env = builder.HostEnvironment
+        printfn "HostEnvironment %s BaseAddress %s" env.Environment env.BaseAddress
+        let http = new HttpClient(BaseAddress = Uri(env.BaseAddress))
+        builder.Services.AddSingleton<HttpClient>(http) |> ignore
 
         builder.RootComponents.Add<Main.MyApp>("#main")
 #if DEBUG
         builder.Logging.SetMinimumLevel(LogLevel.Debug) |> ignore
-        builder.Services.AddRemoting(builder.HostEnvironment) |> ignore
+        builder.Services.AddRemoting(env) |> ignore
 #endif        
         builder.Build().RunAsync() |> ignore
         0
