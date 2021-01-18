b39dbe09b1fbddbb3779c6878ade1bccd93fa628
Craig Wilson <craig@wilsoncg.net>
2021-01-18 15:43:21 +0000

    try to scroll on incoming message

--- src/BoleroGitHub.Client/wwwroot/js/webChat.js
+++ src/BoleroGitHub.Client/wwwroot/js/webChat.js
@@ -4,13 +4,29 @@ window.chat = () =>
             const res = await fetch('https://rachaelazurefunction.azurewebsites.net/api/Token', 
               { method: 'POST' });
             const { token } = await res.json();
+
+            const store = window.WebChat.createStore({}, ({ dispatch }) => next => action => {
+              if (action.type === 'DIRECT_LINE/INCOMING_ACTIVITY') {
+                const event = new Event('webchatincomingactivity');
+    
+                event.data = action.payload.activity;
+                window.dispatchEvent(event);
+              }
+    
+              return next(action);
+            });
     
             window.WebChat.renderWebChat(
               {
-                directLine: await window.WebChat.createDirectLine({ token })
+                directLine: await window.WebChat.createDirectLine({ token }),
+                store
               },
-              document.getElementById('webchat')
+              document.getElementById('webchat')             
             );
+
+            window.addEventListener('webchatincomingactivity', ({ data }) => {
+              document.getElementById('main-content').scrollIntoView(false);
+            });
     
             document.querySelector('#webchat > *').focus();
           })().catch(err => console.error(err));
